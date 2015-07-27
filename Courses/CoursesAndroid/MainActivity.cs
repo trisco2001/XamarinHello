f216349f2567ae9699798771ed3f291c1b999339
Tristan Leonard <tristanl@slalom.com>
2015-07-27 11:09:31 -0700

    Initial Commit

---
+++ Courses/CoursesAndroid/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace CoursesAndroid
+{
+    [Activity(Label = "Courses", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button buttonPrev;
+        Button buttonNext;
+        TextView textTitle;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            buttonPrev = FindViewById<Button>(Resource.Id.buttonPrev);
+            buttonNext = FindViewById<Button>(Resource.Id.buttonNext);
+            textTitle = FindViewById<TextView>(Resource.Id.textTitle);
+
+            buttonPrev.Click += ButtonPrev_Click;
+            buttonNext.Click += ButtonNext_Click;
+        
+        }
+
+        private void ButtonPrev_Click(object sender, EventArgs e)
+        {
+            textTitle.Text = "Previous Clicked";
+        }
+
+        private void ButtonNext_Click(object sender, EventArgs e)
+        {
+            textTitle.Text = "Next Clicked";
+        }
+    }
+}
+
